0f904e44b205eb5527f25539428ae3011b5c3f97
DANG BA MINH <d.baminh@gmail.com>
2024-10-28 21:11:37 +1100

    feat: add test case for lex

--- tests/test_lex.py
+++ tests/test_lex.py
@@ -2,13 +2,21 @@ import importlib.resources
 from pcc import lex
 import importlib
 from tests import resources
+import pytest
 
 
 class TestLexer:
-  def test_tokenize(self):
+  def test_tokenize_succeed(self):
     lexer = lex.Lexer()
     path = importlib.resources.files(resources).joinpath("multi_digit.c")
     tokens = []
     with open(path) as file:
       tokens = lexer.tokenize(file.read())
     assert len(tokens) == 10
+
+  @pytest.mark.xfail
+  def test_tokenize_fail(self):
+    path = importlib.resources.files(resources).joinpath("at_sign.c")
+    lexer = lex.Lexer()
+    with open(path) as file:
+      lexer.tokenize(file.read())
