b1194c9acb5071fdd6ca98ae3fd6536dda8acddf
DANG BA MINH <d.baminh@gmail.com>
2024-11-09 11:21:36 +1100

    feat: remove exception handler to make debug process easier

--- driver.py
+++ driver.py
@@ -15,26 +15,20 @@ if __name__ == "__main__":
   argparser.add_argument("--parse", action="store_true", help="Perform up to parse pass")
   argparser.add_argument("--codegen", action="store_true", help="Perform up to codegen pass")
   args = argparser.parse_args()
-
   if args.parse or args.lex or args.codegen:
     tokens = []
     with open(args.filename, "r") as f:
       text = f.read()
       lexer = lex.Lexer()
-      try:
-        tokens = lexer.tokenize(text)
-      except ValueError:
-        exit(1)
+      tokens = lexer.tokenize(text)
 
     if args.parse or args.codegen:
       parser = parse.Parser(tokens)
       program_ast = parser.parse_program()
 
       if args.codegen:
-        print("here")
         code_generator = assembly_gen.AssemblyGenerator(program_ast)
         program_aast = code_generator.parse_program_ast()
-        output_file = os.path.dirname(args.filename) + "/" + os.path.basename(args.filename)
-        print(output_file)
+        output_file = os.path.basename(args.filename)[:-2]
         code_emitter = code_emitter.CodeEmitter(output_file, program_aast)
         code_emitter.emit()
